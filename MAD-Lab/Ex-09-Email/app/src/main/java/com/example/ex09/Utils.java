5ffd9acad91e641346597af46a24554793970a36
SoundharyaDGRT <soundnext123@gmail.com>
2021-10-02 10:47:44 +0530

    EMAIL

---
+++ MAD-Lab/Ex-09-Email/app/src/main/java/com/example/ex09/Utils.java
@@ -0,0 +1,8 @@
+package com.example.ex09;
+
+public class Utils {
+    // HERE PUT EMAIL AND PASSWORD , LIKE example@gmail.com
+
+    public static final  String EMAIL = "your email address";
+    public static final String PASSWORD = "password";
+}
